a44affed72a141fb8be428bbb6c66e529fd2e165
Muteeba4626 <muteeba.shahzad26@gmail.com>
2026-01-04 23:30:57 +0500

    Time: 70 ms (21.91%), Space: 15.7 MB (70.92%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,46 @@
+// LeetCode 44 - Wildcard Matching
+// https://leetcode.com/problems/wildcard-matching/
+// Author: muteeba4266
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] means s[0...i-1] matches p[0...j-1]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Handle patterns starting with '*' matching empty string
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 1];
+            } else {
+                break; 
+            }
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                    // Current characters match or '?' matches any single char
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    // '*' can match empty sequence (dp[i][j-1]) 
+                    // or one/more characters (dp[i-1][j])
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
