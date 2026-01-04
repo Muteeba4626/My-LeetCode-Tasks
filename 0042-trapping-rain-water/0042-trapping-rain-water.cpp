33e018c8ff75038cef0b1f6857d41de285161841
Muteeba4626 <muteeba.shahzad26@gmail.com>
2026-01-04 23:30:11 +0500

    Time: 0 ms (100%), Space: 26.1 MB (47.34%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,42 @@
+// LeetCode 42 - Trapping Rain Water
+// https://leetcode.com/problems/trapping-rain-water/
+// Author: muteeba4266
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) return 0;
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int trappedWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // Update leftMax and add trapped water at left pointer
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // Update rightMax and add trapped water at right pointer
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return trappedWater;
+    }
+};
