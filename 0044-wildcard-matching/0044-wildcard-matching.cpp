8a6c44dde7a5455d6525a7c8a7da6cd154ae758d
Muteeba4626 <muteeba.shahzad26@gmail.com>
2026-01-04 23:31:19 +0500

    Time: 66 ms (25.81%), Space: 15.9 MB (42.82%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,11 +2,6 @@
 // https://leetcode.com/problems/wildcard-matching/
 // Author: muteeba4266
 
-#include <vector>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     bool isMatch(string s, string p) {
