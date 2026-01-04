40e2ab3288fa05bbedabe30a5608f7701bf373df
Muteeba4626 <muteeba.shahzad26@gmail.com>
2026-01-04 23:08:02 +0500

    Time: 5 ms (37.09%), Space: 18.5 MB (64.6%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,46 @@
+// LeetCode 23 - Merge k Sorted Lists
+// https://leetcode.com/problems/merge-k-sorted-lists/
+// Author: muteeba4266
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> minHeap;
+
+        for (ListNode* list : lists) {
+            if (list) {
+                minHeap.push(list);
+            }
+        }
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (!minHeap.empty()) {
+            ListNode* smallest = minHeap.top();
+            minHeap.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if (smallest->next) {
+                minHeap.push(smallest->next);
+            }
+        }
+
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
+};
